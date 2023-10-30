85ad486bb71005906092c0a4a9a7f3e4dc3491d5
bllee29 <bllee0209@gmail.com>
2023-10-30 18:31:56 +0900

    firebase storage test

--- camera.py
+++ camera.py
@@ -24,7 +24,7 @@ def fileUpload(file):
     print(blob.public_url.split('/')[-1])
 
 def camera_snapshot():
-    subtitle = "rasp_camera_image/MTUWK6P8BIkRavItd4vX" # 직접 지정해준 Directory/UID
+    subtitle = "camera_image/adea78a3388b48afba01ead2b0830b55" # 직접 지정해준 Directory/UID
     suffix = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + '.jpg' # 현재시간
     filename = "_".join([subtitle, suffix]) # 둘을 '_' 기준으로 합침
     subprocess.call("libcamera-jpeg -o {}".format(filename), shell=True) # Shell Script 실행 (libcamera 이용)
