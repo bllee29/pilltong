12561a7277c3c4e44e6dede1208845dd72b8da0f
bllee29 <bllee0209@gmail.com>
2023-10-31 23:02:36 +0900

    tmp

--- camera.py
+++ camera.py
@@ -3,11 +3,13 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
 from uuid import uuid4
-import subprocess # For using Shell Script
+import subprocess # Shell Script
 
-PROJECT_ID = "pilltong-9b8cd" # Owner Project ID
+# 프로젝트 id
+PROJECT_ID = "pilltong-9b8cd" 
 
-cred = credentials.Certificate("./Key/serviceKey.json") # Service Key Path
+# firebase에 연동하기 위한 Key의 디렉토리
+cred = credentials.Certificate("./Key/serviceKey.json") 
 
 default_app = firebase_admin.initialize_app(cred, {
     'storageBucket': f"{PROJECT_ID}.appspot.com"
@@ -24,12 +26,12 @@ def fileUpload(file):
     print(blob.public_url.split('/')[-1])
 
 def camera_snapshot():
-    subtitle = "camera_image/adea78a3388b48afba01ead2b0830b55" # 직접 지정해준 Directory/UID
+    subtitle = "camera_image/adea78a3388b48afba01ead2b0830b55" # Directory/UID
     suffix = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + '.jpg' # 현재시간
-    filename = "_".join([subtitle, suffix]) # 둘을 '_' 기준으로 합침
-    subprocess.call("libcamera-jpeg -o {}".format(filename), shell=True) # Shell Script 실행 (libcamera 이용)
+    filename = "_".join([subtitle, suffix]) # '_' 기준으로 합침
+    subprocess.call("libcamera-jpeg -o {}".format(filename), shell=True) # Shell Script(libcamera)
     fileUpload(filename)
 
-print("Start ......")
+print("Start")
 camera_snapshot()
-print("End Of File")
+print("End")
