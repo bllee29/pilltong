694e558096102153850ff5243c283e941779e6e9
bllee29 <bllee0209@gmail.com>
2023-11-01 01:35:14 +0900

    gpiotest

---
+++ blink.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <wiringPi.h>
+  
+#define LED1 17 
+#define LED2 13 
+  
+int main(){      
+    if(wiringPiSetupGpio() < 0){ 
+         printf("Unable to setup wiring Pi");   
+        return 1;                                
+      }
+ 
+    pinMode(LED1, OUTPUT);      
+    pinMode(LED2, OUTPUT);      
+ 
+    for(;;){                 
+          digitalWrite(LED1, 1);    
+          delay(1000);             
+          digitalWrite(LED1, 0);    
+          delay(1000);               
+          digitalWrite(LED2, 1);    
+          delay(1000);              
+          digitalWrite(LED2, 0);   
+          delay(1000);             
+ 
+    return 0;
+  }
+}
