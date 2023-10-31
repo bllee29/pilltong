fac26fbab99e8010eba7618f3584ac4ce3d26b17
bllee29 <bllee0209@gmail.com>
2023-11-01 01:37:24 +0900

    gpiotest

--- blink.c
+++ blink.c
@@ -10,18 +10,13 @@ int main(){
         return 1;                                
       }
  
-    pinMode(LED1, OUTPUT);      
-    pinMode(LED2, OUTPUT);      
+    pinMode(LED1, OUTPUT);         
  
     for(;;){                 
           digitalWrite(LED1, 1);    
           delay(1000);             
           digitalWrite(LED1, 0);    
-          delay(1000);               
-          digitalWrite(LED2, 1);    
-          delay(1000);              
-          digitalWrite(LED2, 0);   
-          delay(1000);             
+          delay(1000);                           
  
     return 0;
   }
