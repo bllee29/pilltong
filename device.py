fadbe8c4930103b3a88e9cb45cf495ee081dea5f
bllee29 <bllee0209@gmail.com>
2023-11-24 16:15:18 +0900

    suffix change & config file loading

--- device.py
+++ device.py
@@ -1,20 +1,29 @@
-import datetime
 import subprocess
 import ctypes
+import json
+
+def setting() -> str:
+    with open ("config.json", 'r') as f:
+        config = json.load(f)
+    return config
 
 # use absolute path for using in /etc/rc.local
 dev = ctypes.CDLL("./home/pilltong/pilltong/device.so")
+config = setting()
 
 def modeSelect() -> int:
     return dev.userInput()
 
-def camera_snapshot(UID: str, bright: int) -> str:
+def camera_snapshot(UID: str, suffix: str, bright: int) -> str:
     dev.LEDON(bright);
-    prefix = f"device_image/{UID}/FindPill"
-    suffix = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + '.jpeg'
-    filename = "_".join([prefix, suffix])
+    prefix = f"device_image/{UID}/"
+    suffix = suffix + f"{bright}" + ".jpeg"
+    filename = "".join([prefix, suffix])
     subprocess.call("libcamera-jpeg -o {}".format(filename), shell=True)
     return filename
+
+def turn():
+    dev.turn()
 
 def endProc():
     subprocess.call("sudo shutdown now {}", shell=True)
